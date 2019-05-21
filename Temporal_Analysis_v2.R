eb86292bfc477c1b7d014a62f77af6a7e412c414
Nithishwer M. A <31736866+Nithishwer@users.noreply.github.com>
2019-05-21 13:19:03 +0530

    Update Temporal_Analysis_v2.R

--- Temporal_Analysis_v2.R
+++ Temporal_Analysis_v2.R
@@ -1,5 +1,5 @@
 library(readxl)
-Data_1Y <- read_excel("/home/pennyworth/Documents/Bus/Data_One Year_200 m.xlsx")
+Data_1Y <- read_excel("somefile.xlsx")
 #View(Data_1Y)
 Data_1Y$td=(Data_1Y$...3)/3600
 Data_1Y$tdr=as.integer(Data_1Y$td)
@@ -21,29 +21,3 @@ for (ti in 1:24){
 #range(Data_1Y$tdr) is screwed up      
    
 }
- 
-
-# 
-# for (ti in 1:24){
-#   ti_df = df24[FALSE,]
-#   index=0
-#   for (row in 1:148) 
-#   {
-#     t=Data_1Y[row,"td"]  # Works Fine 
-#     t_24=as.integer(t)   # Works Fine
-#     if (ti == t_24){     
-#       index=index+1      
-#       for (seg in 1:5)   # Works Fine
-#       {
-#         ti_df[index,seg]=Data_1Y[row,seg+3]
-#       }
-#     
-#     }
-#   }
-#   for (col in 1:ncol(ti_df)){
-#     mn=mean(ti_df[[col]])
-#     df24[t_24+1,col]=mn
-#   }
-# }
-
-# Works fine for individual ti values
