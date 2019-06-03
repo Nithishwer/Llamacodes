0051b32c2655bb27cb0542d3c90b74d8fc6c1391
Nithishwer M. A <31736866+Nithishwer@users.noreply.github.com>
2019-06-03 16:25:53 +0530

    Update Temporal_Analysis_v2.R
    Excluding the Summed time

--- Temporal_Analysis_v2.R
+++ Temporal_Analysis_v2.R
@@ -4,13 +4,13 @@ View(Data_1Y)
 Data_1Y$td=(Data_1Y$...3)/3600
 #Data_1Y$tdr=as.integer(Data_1Y$td)
 #View(small_data)
-M24=matrix(NA, nrow = 24, ncol = 148)
+M24=matrix(NA, nrow = 24, ncol = 147)
 df24=as.data.frame(M24)
 #View(df24)
 for (ti in 1:24){
   
   sel<-as.data.frame(subset(Data_1Y,td<ti & td>=ti-1))
-  sel<-sel[,1:(ncol(sel)-1)]
+  sel<-sel[,1:(ncol(sel)-2)]
   sel<-sel[,4:ncol(sel)]
   if (dim(sel)[1]!=0){
     
@@ -29,13 +29,13 @@ heatmap(m24, Colv = NA, Rowv = NA, scale="column")
 #  .5 Hr Heatmap
 # ##############
 
-M48=matrix(NA, nrow = 48, ncol = 148)
+M48=matrix(NA, nrow = 48, ncol = 147)
 df48=as.data.frame(M48)
 #View(df24)
 for (ti in 1:48){
   
   sel<-as.data.frame(subset(Data_1Y,td<ti/2 & td>=(ti-1)/2))
-  sel<-sel[,1:(ncol(sel)-1)]
+  sel<-sel[,1:(ncol(sel)-2)]
   sel<-sel[,4:ncol(sel)]
   if (dim(sel)[1]!=0){
     
@@ -55,13 +55,13 @@ heatmap(m48, Colv = NA, Rowv = NA, scale="column")
 #  .25 Hr Heatmap
 # ###############
 
-M96=matrix(NA, nrow = 96, ncol = 148)
+M96=matrix(NA, nrow = 96, ncol = 147)
 df96=as.data.frame(M96)
 #View(df24)
 for (ti in 1:96){
   
   sel<-as.data.frame(subset(Data_1Y,td>=(ti-1)/4 & td<ti/4))
-  sel<-sel[,1:(ncol(sel)-1)]
+  sel<-sel[,1:(ncol(sel)-2)]
   sel<-sel[,4:ncol(sel)]
   if (dim(sel)[1]!=0){
     
@@ -87,13 +87,13 @@ Data_1Y <- read_excel("somefile.xlsx")
 Data_1Y$m<- as.numeric(substring(Data_1Y$...1,6,7))
 #Data_1Y$tdr=as.integer(Data_1Y$td)
 #View(small_data)
-M12=matrix(NA, nrow = 12, ncol = 148)
+M12=matrix(NA, nrow = 12, ncol = 147)
 df12=as.data.frame(M12)
 #View(df12)
 for (mi in 1:12){
   
   sel<-as.data.frame(subset(Data_1Y,m==mi))
-  sel<-sel[,1:(ncol(sel)-2)]
+  sel<-sel[,1:(ncol(sel)-3)]
   sel<-sel[,4:ncol(sel)]
   if (dim(sel)[1]!=0){
     
@@ -115,13 +115,13 @@ heatmap(m12, Colv = NA, Rowv = NA, scale="column")
 Data_1Y <- read_excel("somefile.xlsx")
 #View(Data_1Y)
 Data_1Y$day<- as.POSIXlt(Data_1Y$...1)$wday
-M7=matrix(NA, nrow = 7, ncol = 148)
+M7=matrix(NA, nrow = 7, ncol = 147)
 df7=as.data.frame(M7)
 View(df7)
 for (di in 0:6){
   
   sel<-as.data.frame(subset(Data_1Y,day==di))
-  sel<-sel[,1:(ncol(sel)-3)]
+  sel<-sel[,1:(ncol(sel)-4)]
   sel<-sel[,4:ncol(sel)]
   if (dim(sel)[1]!=0){
     
