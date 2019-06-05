640012cbd1e449b8e41111b042ffcaf8abe46b0c
Nithishwer M. A <31736866+Nithishwer@users.noreply.github.com>
2019-06-05 19:22:23 +0530

    Add files via upload

--- Temporal_Analysis_v3.R
+++ Temporal_Analysis_v3.R
@@ -2,7 +2,7 @@ library(readxl)
 library(reshape2)
 library(ggplot2)
 library(RColorBrewer)
-Data_1Y <- read_excel("/home/dinofelis/Downloads/Traffic/Data/Data_1Y_raw.xlsx")
+Data_1Y <- read_excel("somefile.xlsx")
 View(Data_1Y)
 
 # Add a column corresponding to time in Hrs
@@ -36,14 +36,19 @@ for (ti in 1:24){
 #View(df24)
 df24=df24[-c(2,3,24),]
 
-# Melting the df24 dataframe to help with the heatmap plotting and using the ggplot package
+#Melting the df24 dataframe to help with the heatmap plotting and using the ggplot package
 molten_df24=melt(df24, id = c("V148"))
 View(molten_df24)
 
+#Binning the data in order to plot ranges in HM
+molten_df24$valuefactor<-cut(molten_df24$value,breaks=c(0,25,50,75,100,max(molten_df24$value)),labels = c("0-25","25-50","50-75","75-100",">100"))
+
 #Plotting the heatmap with ggplot
-df24_heatmap <- ggplot(data = molten_df24, mapping = aes(x = variable, y = V148,fill = value)) + geom_tile() + #geom_title specifies that it is a heatmap
+df24_heatmap <- ggplot(data = molten_df24, mapping = aes(x = variable, y = V148,fill = valuefactor)) + geom_tile() + #geom_title specifies that it is a heatmap
                               xlab(label = "Road Segment") + ylab(label = "Time in Hours")+ggtitle(label = "Heatmap for Hourly bins") +
-                              scale_fill_distiller(name=" Time in seconds", palette = "RdYlBu") # Scale_fill_distiller is from Rbrewer package and now uses the RdBu palette
+                              scale_x_discrete(expand = c(0,0), breaks=c("V1","V25","V50","V75","V100","V125"))+ # To fix the labels in the x axis
+                              scale_fill_manual(values = rev(brewer.pal(n = 5,name = "RdYlGn")),name="Travel time (s)") # Scale_fill_manual is used for discrete filling
+#                             scale_fill_distiller(name=expression(Time ~ (seconds^{-3})), palette = "RdYlBu") # Scale_fill_distiller is from Rbrewer package and for continuos fill
 df24_heatmap
 
 # ##########################################################################
@@ -67,11 +72,14 @@ for (ti in 1:48){
   }
   
 }
+df48<-df48[-c(2,3,4,5,6,7,47,48),]
 molten_df48=melt(df48, id = c("V148"))
 View(molten_df48)
-df48_heatmap <- ggplot(data = molten_df48, mapping = aes(x = variable, y = V148,fill = value)) + geom_tile() + 
+molten_df48$valuefactor<-cut(molten_df48$value,breaks=c(0,25,50,75,100,max(molten_df48$value)),labels = c("0-25","25-50","50-75","75-100",">100"))
+df48_heatmap <- ggplot(data = molten_df48, mapping = aes(x = variable, y = V148,fill = valuefactor)) + geom_tile() + 
   xlab(label = "Road Segment") + ylab(label = "Time in 0.5 Hours")+ggtitle(label = "Heatmap for 30 Minute bins") +
-  scale_fill_distiller(name=" Time in seconds", palette = "RdYlBu") 
+  scale_x_discrete(expand = c(0,0), breaks=c("V1","V25","V50","V75","V100","V125"))+
+  scale_fill_manual(values = rev(brewer.pal(n = 5,name = "RdYlGn")),name="Travel time (s)") # Scale_fill_distiller is from Rbrewer package and now uses the RdBu palette
 df48_heatmap
 
 # ##########################################################################
@@ -98,9 +106,12 @@ for (ti in 1:96){
 
 df96=df96[-c(c(2:15),93:96,19),]
 molten_df96=melt(df96, id = c("V148"))
-df96_heatmap <- ggplot(data = molten_df96, mapping = aes(x = variable, y = V148,fill = value)) + geom_tile() + 
+molten_df96$valuefactor<-cut(molten_df96$value,breaks=c(0,25,50,75,100,max(molten_df96$value)),labels = c("0-25","25-50","50-75","75-100",">100"))
+df96_heatmap <- ggplot(data = molten_df96, mapping = aes(x = variable, y = V148,fill = valuefactor)) + geom_tile() + 
   xlab(label = "Road Segment") + ylab(label = "Time in 0.25 Hours")+ggtitle(label = "Heatmap for 15 Minute bins") +
-  scale_fill_distiller(name=" Time in seconds", palette = "RdYlBu") 
+  scale_x_discrete(expand = c(0,0), breaks=c("V1","V25","V50","V75","V100","V125"))+
+  scale_fill_manual(values = rev(brewer.pal(n = 5,name = "RdYlGn")),name="Travel time (s)") # Scale_fill_distiller is from Rbrewer package and now uses the RdBu palette
+
 df96_heatmap
 
 
@@ -108,7 +119,7 @@ df96_heatmap
 #  12months Heatmap
 # #################
 
-Data_1Y <- read_excel("/home/dinofelis/Downloads/Traffic/Data/Data_1Y_raw.xlsx")
+Data_1Y <- read_excel("somefile.xlsx")
 #View(Data_1Y)
 Data_1Y$m<- as.numeric(substring(Data_1Y$...1,6,7))
 #Data_1Y$tdr=as.integer(Data_1Y$td)
@@ -130,18 +141,25 @@ for (mi in 1:12){
   }
   
 }
+
+#Something to fix y axis labels
+number_ticks<-function(n){function(limits) pretty(limits,n)}
+
 #df24=df24[-c(2,3,24),]
 molten_df12=melt(df12, id = c("V148"))
-df12_heatmap <- ggplot(data = molten_df12, mapping = aes(x = variable, y = V148,fill = value)) + geom_tile() + 
+molten_df12$valuefactor<-cut(molten_df12$value,breaks=c(0,25,50,75,100,max(molten_df12$value)),labels = c("0-25","25-50","50-75","75-100",">100"))
+df12_heatmap <- ggplot(data = molten_df12, mapping = aes(x = variable, y = V148,fill = valuefactor)) + geom_tile() + 
   xlab(label = "Road Segment") + ylab(label = "Month Number")+ggtitle(label = "Heatmap for Monthly bins") +
-  scale_fill_distiller(name=" Time in seconds", palette = "RdYlBu") 
+  scale_x_discrete(expand = c(0,0), breaks=c("V1","V25","V50","V75","V100","V125"))+
+  scale_y_continuous(breaks=number_ticks(8))+
+  scale_fill_manual(values = rev(brewer.pal(n = 5,name = "RdYlGn")),name="Travel time (s)") # Scale_fill_distiller is from Rbrewer package and now uses the RdBu palette
 df12_heatmap
 
 # ##########################################################################
 #  Weekday Heatmap
 # #################
 
-Data_1Y <- read_excel("/home/dinofelis/Downloads/Traffic/Data/Data_1Y_raw.xlsx")
+Data_1Y <- read_excel("somefile.xlsx")
 #View(Data_1Y)
 Data_1Y$day<- as.POSIXlt(Data_1Y$...1)$wday
 M7=matrix(NA, nrow = 7, ncol = 148)
@@ -163,7 +181,10 @@ for (di in 0:6){
 }
 m7=as.matrix(df7)
 molten_df7=melt(df7, id = c("V148"))
-df7_heatmap <- ggplot(data = molten_df7, mapping = aes(x = variable, y = V148,fill = value)) + geom_tile() + 
+molten_df7$valuefactor<-cut(molten_df7$value,breaks=c(0,25,50,75,100,max(molten_df7$value)),labels = c("0-25","25-50","50-75","75-100",">100"))
+df7_heatmap <- ggplot(data = molten_df7, mapping = aes(x = variable, y = V148,fill = valuefactor)) + geom_tile() + 
   xlab(label = "Road Segment") + ylab(label = "Day Number")+ggtitle(label = "Heatmap for Weekday bins") +
-  scale_fill_distiller(name=" Time in seconds", palette = "RdYlBu") 
+  scale_x_discrete(expand = c(0,0), breaks=c("V1","V25","V50","V75","V100","V125"))+
+  scale_y_continuous(breaks=number_ticks(8))+
+  scale_fill_manual(values = rev(brewer.pal(n = 5,name = "RdYlGn")),name="Travel time (s)") # Scale_fill_distiller is from Rbrewer package and now uses the RdBu palette
 df7_heatmap
